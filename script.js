04ceb0147aeebb248a369f1db183ce05a1610f78
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-09 12:47:55 -0300

    CEP API

---
+++ script.js
@@ -0,0 +1,34 @@
+async function buscarEndereco(cep) {
+
+  let mensagemErro = document.querySelector('#erro');
+  mensagemErro.innerHTML = '';
+
+  try {
+    let consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    let consultaCepConvertida = await consultaCep.json();
+
+    if (consultaCepConvertida.erro) {
+      throw Error('CEP não existe!');
+    }
+
+    let cidade = document.querySelector('#cidade');
+    let endereco = document.querySelector('#endereco');
+    let estado = document.querySelector('#estado');
+    let bairro = document.querySelector('#bairro');
+
+    cidade.value = consultaCepConvertida.localidade;
+    endereco.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+    bairro.value = consultaCepConvertida.bairro;
+
+    console.log(consultaCepConvertida);
+    return consultaCepConvertida;
+
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+    console.log(erro);
+  }
+}
+
+let cep = document.querySelector('#cep');
+cep.addEventListener('focusout', () => buscarEndereco(cep.value));
